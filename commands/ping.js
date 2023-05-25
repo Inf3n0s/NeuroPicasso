88177a719e0084a7e4e900aa82aa371ae97d66c2
Scânteie Alexandru <alexscanteie0808@gmail.com>
2023-05-25 10:34:13 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,8 @@
 const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-  data: new SlashCommandBuilder().setName("scant").setDescription("Scant"),
+  data: new SlashCommandBuilder().setName("salut").setDescription("Salută comisia"),
   async execute(interaction) {
-    interaction.reply("Scant love you!");
+    interaction.reply("Salutare, comisie! Folosiți comanda /imagine pentru a vă genera propria imagine. Vă mulțumesc pentru încredere și colaborare!");
   },
 };
